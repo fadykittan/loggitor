461c177b02b70474587a21823a3ed2e2a2fadbcf
Fady Kittan <fady.kittan@gmail.com>
2019-01-03 03:18:40 +0200

    first commit for this project

---
+++ src/main/java/com/loggitor/v2/loggitor/LoggitorApplication.java
@@ -0,0 +1,17 @@
+package com.loggitor.v2.loggitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggitorApplication.class, args);
+		
+		
+		
+	}
+
+}
+
